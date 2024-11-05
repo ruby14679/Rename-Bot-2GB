3a71c02771540e93bd8281363be3ee753d3783bf
ruby14679 <mrobi79@gmail.com>
2024-11-05 11:53:32 +0800

    Update force_subs.py

--- plugins/force_subs.py
+++ plugins/force_subs.py
@@ -23,8 +23,8 @@ async def not_subscribed(_, client, message):
 
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}") ]]
-    text = f"""<b>Hello {message.from_user.mention} \n\nYou Need To Join In My Channel To Use Me\n\nKindly Please Join Channel</b>"""
+    buttons = [[InlineKeyboardButton(text="📢 Join My Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}") ]]
+    text = f"""<b>Hello {message.from_user.mention} \n\nPlease Join My Channel To Use Me</b>"""
     try:
         user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
         if user.status == enums.ChatMemberStatus.BANNED:                                   
